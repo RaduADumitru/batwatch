2beacc3198c5a8454f4f312e2a175d81097d2fc6
RaduADumitru <radu.dumitru@rocketmail.com>
2026-01-03 14:27:57 +0200

    Application skeleton

---
+++ src/main/java/com/radud/batwatch/BatwatchApplication.java
@@ -0,0 +1,13 @@
+package com.radud.batwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatwatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatwatchApplication.class, args);
+    }
+
+}
