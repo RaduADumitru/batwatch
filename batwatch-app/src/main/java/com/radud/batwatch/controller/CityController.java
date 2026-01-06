d6556489a89a6324727b78235e885609aca584cb
RaduADumitru <radu.dumitru@rocketmail.com>
2026-01-06 19:26:59 +0200

    Restrict adding city to admins

--- batwatch-app/src/main/java/com/radud/batwatch/controller/CityController.java
+++ batwatch-app/src/main/java/com/radud/batwatch/controller/CityController.java
@@ -8,6 +8,7 @@ import com.radud.batwatch.service.CityService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class CityController {
     private final CityMapper cityMapper;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> createCity(@Valid @RequestBody CreateCityRequest request) {
         City city = cityMapper.toModel(request);
         City createdCity = cityService.createCity(city);
