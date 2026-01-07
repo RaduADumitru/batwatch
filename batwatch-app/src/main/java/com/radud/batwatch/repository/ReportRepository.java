34ff111c68849ce32c478aa11ceeab569269c592
RaduADumitru <radu.dumitru@rocketmail.com>
2026-01-07 22:32:48 +0200

    Clean up query

--- batwatch-app/src/main/java/com/radud/batwatch/repository/ReportRepository.java
+++ batwatch-app/src/main/java/com/radud/batwatch/repository/ReportRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
     @Query(value = "SELECT distinct report.* FROM report " +
-            "JOIN location on report.id = location.report_id WHERE ST_DWithin(location.location_point::geography, ST_SetSRID(ST_MakePoint(?1, ?2), 4326)::geography, ?3)", nativeQuery = true)
+            "JOIN location on report.id = location.report_id " +
+            "WHERE ST_DWithin(location.location_point::geography, " +
+            "ST_SetSRID(ST_MakePoint(?1, ?2), 4326)::geography, ?3)",
+            nativeQuery = true)
     List<Report> findByCityId(Double longitude, Double latitude, Long radiusInMeters);
 }
